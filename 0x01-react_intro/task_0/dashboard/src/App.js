348b165303354922ed7b39f477ad4442e5e5ff40
xoluxp <xoluxp@gmail.com>
2024-11-04 03:46:10 +0000

    happy

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,21 +1,22 @@
-import logo from './holberton-logo.jpg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Create this file if it doesn’t exist and style as needed
+import holbertonLogo from './holberton-logo.png'; // Update filename based on actual logo name
 
 function App() {
-	return (
-		<div className='App'>
-			<div className='App-header'>
-				<img src={logo} alt='Holberton' />
-				<h1>School dashboard</h1>
-			</div>
-			<div className='App-body'>
-				<p>Login to access the full dashboard</p>
-			</div>
-			<div className='App-footer'>
-				<p>Copyright 2020 - holberton School</p>
-			</div>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holbertonLogo} alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <main className="App-body">
+        <p>Login to access the full dashboard</p>
+      </main>
+      <footer className="App-footer">
+        <p>Copyright 2020 - Holberton School</p>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
