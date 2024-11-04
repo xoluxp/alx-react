58377ca4ff2cc87088939b11df0a311c2064407d
xoluxp <xoluxp@gmail.com>
2024-11-04 04:16:20 +0000

    task 1 done

--- 0x01-react_intro/task_1/dashboard/src/App.js
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -1,24 +1,28 @@
-import logo from './holberton-logo.jpg';
+// src/App.js
+
+import React from 'react';
 import './App.css';
+import holbertonLogo from './holberton-logo.png';
 import { getFullYear, getFooterCopy } from './utils';
 
 function App() {
-	return (
-		<div className='App'>
-			<div className='App-header'>
-				<img src={logo} alt='Holberton' />
-				<h1>School dashboard</h1>
-			</div>
-			<div className='App-body'>
-				<p>Login to access the full dashboard</p>
-			</div>
-			<div className='App-footer'>
-				<p>
-					Copyright {getFullYear()} - {getFooterCopy(true)}
-				</p>
-			</div>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holbertonLogo} alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <main className="App-body">
+        <p>Login to access the full dashboard</p>
+      </main>
+      <footer className="App-footer">
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(false)}
+        </p>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
+
