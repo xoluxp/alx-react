db248e36d3628663770bc3ee413b8b67e733c0b5
xoluxp <xoluxp@gmail.com>
2024-11-05 02:07:24 +0000

    happy

--- 0x01-react_intro/task_2/dashboard/src/index.js
+++ 0x01-react_intro/task_2/dashboard/src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Notifications from './Notifications';
+import { Notifications } from './Notifications';
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 ReactDOM.render(
-	<React.StrictMode>
-		<Notifications />
-	</React.StrictMode>,
-	document.getElementById('root-notifications')
+  <React.StrictMode>
+    <Notifications />
+  </React.StrictMode>,
+  document.getElementById('root-notifications')
 );
